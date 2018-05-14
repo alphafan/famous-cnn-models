292ee373f8ac5c853aec735454034aaa0978dbc6
Yufan Zheng <yufan.zheng1@gmail.com>
2018-05-14 23:57:56 +0800

    download

--- utils/load_image_net.py
+++ utils/load_image_net.py
@@ -1,15 +1,31 @@
 import os
+import tarfile
+import gzip
 
 from pathlib import Path
 import requests
 from tqdm import tqdm
 
 # ImageNet WordNet ID --> image urls mappings download path
-url = 'http://image-net.org/imagenet_data/urls/imagenet_fall11_urls.tgz'
+# ..
+# n00004475_6590   http://farm4.static.flickr.com/3175/2737866473_7958dc8760.jpg
+# n00004475_15899  http://farm4.static.flickr.com/3276/2875184020_9944005d0d.jpg
+# n00004475_32312  http://farm3.static.flickr.com/2531/4094333885_e8462a8338.jpg
+# ..
+wids_url = 'http://image-net.org/imagenet_data/urls/imagenet_fall11_urls.tgz'
+
+# ImageNet WordNet ID --> object type mappings download path
+# ..
+# n00001740   entity
+# n00001930   physical entity
+# n00002137   abstraction, abstract entity
+# ..
+word_url = 'http://image-net.org/archive/words.txt'
 
 # Data directory to save images of ImageNet
 image_net_dir = os.path.join(str(Path(__file__).absolute().parent.parent), 'imageNet')
 image_net_urls_file = os.path.join(image_net_dir, 'imagenet_fall11_urls.tgz')
+image_net_word_file = os.path.join(image_net_dir, 'word.txt')
 image_net_image_dir = os.path.join(image_net_dir, 'images')
 
 # Create these two repository if not exists
@@ -19,16 +35,58 @@ if not os.path.exists(image_net_image_dir):
     os.makedirs(image_net_image_dir)
 
 
-def download_img_urls():
+def load_data(num_images=200, reshape_as=(227, 227)):
+    """ Load ImageNet Dataset
+
+    Args:
+        num_images: ImageNet has 82,114 ids, for fast processing purpose,
+            use only a portion of images for processing
+         reshape_as: Pre-process image to make them all same width and height.
+    """
+    # Download files if not exists
+    if not os.path.exists(image_net_urls_file) or os.path.getsize(image_net_urls_file) != 350302759:
+        _download_img_urls()
+        assert os.path.getsize(image_net_urls_file) == 350302759
+    if not os.path.exists(image_net_word_file) or os.path.getsize(image_net_word_file) != 2655750:
+        _download_word_file()
+        assert os.path.getsize(image_net_word_file) == 2655750
+
+    # Extract datasets
+    with tarfile.open(image_net_urls_file) as tar:
+        print('Extracting imagenet_fall11_urls.tgz...')
+        f = tar.extractfile('fall11_urls.txt')
+        print('Downloading images...')
+        for i in range(num_images):
+            image_id, image_url = f.readline().decode().strip().split('\t')
+            r = requests.get(image_url, stream=True)
+            with open(os.path.join(image_net_image_dir, image_id+'.jpg'), 'wb') as img:
+                total_length = int(r.headers.get('content-length'))
+                for chunk in tqdm(r.iter_content(chunk_size=1024), total=(total_length / 1024) + 1, unit='KB'):
+                    img.write(chunk)
+                    img.flush()
+            print('Downloading', image_id, image_url)
+
+
+def _download_img_urls():
     print('Downloading Image Net WID -> Image Urls Mappings file...')
-    r = requests.get(url, stream=True)
+    r = requests.get(wids_url, stream=True)
     with open(image_net_urls_file, 'wb') as f:
         total_length = int(r.headers.get('content-length'))
         print('Total content length :', total_length)
         for chunk in tqdm(r.iter_content(chunk_size=1024), total=(total_length/1024)+1, unit='KB'):
             f.write(chunk)
             f.flush()
-    print('Download complete')
 
 
-download_img_urls()
+def _download_word_file():
+    print('Downloading Image Net WID -> Object Type Mappings file...')
+    r = requests.get(word_url, stream=True)
+    with open(image_net_word_file, 'wb') as f:
+        total_length = int(r.headers.get('content-length'))
+        print('Total content length :', total_length)
+        for chunk in tqdm(r.iter_content(chunk_size=1024), total=(total_length/1024)+1, unit='KB'):
+            f.write(chunk)
+            f.flush()
+
+
+load_data()
