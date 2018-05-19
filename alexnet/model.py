60d98e2048a9e0d3a2787c3d628d0ac4f40117b6
Yufan Zheng <yufan.zheng1@gmail.com>
2018-05-20 01:13:35 +0800

    change place holder

--- alexnet/model.py
+++ alexnet/model.py
@@ -10,8 +10,8 @@ class AlexNet(object):
 
     def __init__(self):
         # Input & output placeholders
-        self.X = tf.placeholder(dtype=tf.float32, shape=(None, 32, 32, 1), name='image')
-        self.y = tf.placeholder(dtype=tf.float32, shape=(None, 10), name='label')
+        self.X = tf.placeholder(dtype=tf.float32, shape=(None, 227, 227, 3), name='image')
+        self.y = tf.placeholder(dtype=tf.float32, shape=(None, 103), name='label')
 
     def run(self):
         pass
