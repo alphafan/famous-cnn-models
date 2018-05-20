36f0d23c63c000295d84fe1b72194fa84a28d74a
Yufan Zheng <yufan.zheng1@gmail.com>
2018-05-20 13:45:45 +0800

    push

--- alexnet/model.py
+++ alexnet/model.py
@@ -48,38 +48,44 @@ class AlexNet(object):
         # Forward propagation
         ##########################################################################
 
-        # 1st convolutional layer
-        conv1 = tf.nn.conv2d(self.X, self.weights["wc1"], strides=[1, 4, 4, 1], padding="SAME", name="conv1")
-        conv1 = tf.nn.bias_add(conv1, self.biases["bc1"])
-        conv1 = tf.nn.relu(conv1)
-        conv1 = tf.nn.local_response_normalization(conv1, depth_radius=5.0, bias=2.0, alpha=1e-4, beta=0.75)
-        conv1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID")
+        # 1st convolutional layer: Input 227 * 227 * 3  Output 28 * 28 * 96
+        #   - a) Convolution
+        #   - b) Normalization
+        #   - c) Subsampling
+        conv_1 = tf.nn.conv2d(self.X, self.weights["wc1"], strides=[1, 4, 4, 1], padding="SAME", name="conv1")
+        conv_1 = tf.nn.bias_add(conv_1, self.biases["bc1"])
+        conv_1 = tf.nn.relu(conv_1)
+        conv_1 = tf.nn.local_response_normalization(conv_1, depth_radius=5.0, bias=2.0, alpha=1e-4, beta=0.75)
+        conv_1 = tf.nn.max_pool(conv_1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID")
 
-        # 2nd convolutional layer
-        conv2 = tf.nn.conv2d(conv1, self.weights["wc2"], strides=[1, 1, 1, 1], padding="SAME", name="conv2")
-        conv2 = tf.nn.bias_add(conv2, self.biases["bc2"])
-        conv2 = tf.nn.relu(conv2)
-        conv2 = tf.nn.local_response_normalization(conv2, depth_radius=5.0, bias=2.0, alpha=1e-4, beta=0.75)
-        conv2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID")
+        # 2nd convolutional layer: Input 28 * 28 * 96  Output 13 * 13 * 256
+        #   - a) Convolution
+        #   - b) Normalization
+        #   - c) Subsampling
+        conv_2 = tf.nn.conv2d(conv_1, self.weights["wc2"], strides=[1, 1, 1, 1], padding="SAME", name="conv2")
+        conv_2 = tf.nn.bias_add(conv_2, self.biases["bc2"])
+        conv_2 = tf.nn.relu(conv_2)
+        conv_2 = tf.nn.local_response_normalization(conv_2, depth_radius=5.0, bias=2.0, alpha=1e-4, beta=0.75)
+        conv_2 = tf.nn.max_pool(conv_2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID")
 
         # 3rd convolutional layer
-        conv3 = tf.nn.conv2d(conv2, self.weights["wc3"], strides=[1, 1, 1, 1], padding="SAME", name="conv3")
-        conv3 = tf.nn.bias_add(conv3, self.biases["bc3"])
-        conv3 = tf.nn.relu(conv3)
+        conv_3 = tf.nn.conv2d(conv_2, self.weights["wc3"], strides=[1, 1, 1, 1], padding="SAME", name="conv3")
+        conv_3 = tf.nn.bias_add(conv_3, self.biases["bc3"])
+        conv_3 = tf.nn.relu(conv_3)
 
         # 4th convolutional layer
-        conv4 = tf.nn.conv2d(conv3, self.weights["wc4"], strides=[1, 1, 1, 1], padding="SAME", name="conv4")
-        conv4 = tf.nn.bias_add(conv4, self.biases["bc4"])
-        conv4 = tf.nn.relu(conv4)
+        conv_4 = tf.nn.conv2d(conv_3, self.weights["wc4"], strides=[1, 1, 1, 1], padding="SAME", name="conv4")
+        conv_4 = tf.nn.bias_add(conv_4, self.biases["bc4"])
+        conv_4 = tf.nn.relu(conv_4)
 
         # 5th convolutional layer
-        conv5 = tf.nn.conv2d(conv4, self.weights["wc5"], strides=[1, 1, 1, 1], padding="SAME", name="conv5")
-        conv5 = tf.nn.bias_add(conv5, self.biases["bc5"])
-        conv5 = tf.nn.relu(conv5)
-        conv5 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID")
+        conv_5 = tf.nn.conv2d(conv_4, self.weights["wc5"], strides=[1, 1, 1, 1], padding="SAME", name="conv5")
+        conv_5 = tf.nn.bias_add(conv_5, self.biases["bc5"])
+        conv_5 = tf.nn.relu(conv_5)
+        conv_5 = tf.nn.max_pool(conv_5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID")
 
         # stretching out the 5th convolutional layer into a long n-dimensional tensor
-        flatten = tf.layers.flatten(conv5)
+        flatten = tf.layers.flatten(conv_5)
 
         # 1st fully connected layer
         full_1 = tf.layers.dense(flatten, units=4906, activation=tf.nn.tanh)
@@ -99,6 +105,14 @@ class AlexNet(object):
         # Return the complete AlexNet model
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
+            print(sess.run(tf.shape(conv_1), feed_dict={self.X: X_train[:1]}))
+            print(sess.run(tf.shape(conv_2), feed_dict={self.X: X_train[:1]}))
+            print(sess.run(tf.shape(conv_3), feed_dict={self.X: X_train[:1]}))
+            print(sess.run(tf.shape(conv_4), feed_dict={self.X: X_train[:1]}))
+            print(sess.run(tf.shape(conv_5), feed_dict={self.X: X_train[:1]}))
+            print(sess.run(tf.shape(flatten), feed_dict={self.X: X_train[:1]}))
+            print(sess.run(tf.shape(full_1), feed_dict={self.X: X_train[:1]}))
+            print(sess.run(tf.shape(full_2), feed_dict={self.X: X_train[:1]}))
             print(sess.run(tf.shape(full_3), feed_dict={self.X: X_train[:1]}))
 
 
