f90fe2459d4361c801f74ee51d63e496fc4c43b5
Yufan Zheng <yufan.zheng1@gmail.com>
2018-05-22 00:09:41 +0800

    refine

--- googlenet/model.py
+++ googlenet/model.py
@@ -13,47 +13,47 @@ class GoogLeNet(object):
                   conv_55_reduce_size, conv_55_size, pool_conv_size):
         """ Apply inception processing to input tensor.
 
-        An inception layer consists of 4 individual parts and a Concatenation of them
+        An inception layer consists of 4 individual parts and a final concatenation of them
 
-                       Concatenation                            - Part 1:
+                       Concatenation                            - Path 1:
                     /     /     \     \                             a) 1 * 1 Convolutional
                   /      /       \       \
-                /       /         \         \                   - Part 2:
+                /       /         \         \                   - Path 2:
         1 * 1 Conv  3 * 3 Conv  5 * 5 Conv  1 * 1 Conv              a) 1 * 1 Convolutional
             |           |           |          |                    b) 3 * 3 Convolutional
              \          |           |          |
-              \     1 * 1 Conv  1 * 1 Conv  3 * 3 Pool          - Part 3:
+              \     1 * 1 Conv  1 * 1 Conv  3 * 3 Pool          - Path 3:
                 \       |         /       /                         a) 1 * 1 Convolutional
                   \     |      /      /                             b) 5 * 5 Convolutional
                     \   |    /     /
-                      \ |  /   /                                - Part 4:
+                      \ |  /   /                                - Path 4:
                         Inputs                                      a) 3 * 3 Max Pooling
                                                                     b) 1 * 1 Convolutional
 
         Args:
             inputs: (Tensor) -- Input tensor
-            conv_11_size: (int) -- Output dimension of part 1 ( conv 1 * 1 )
-            conv_33_reduce_size: (int) -- Output dimension of patt 2a ( conv 1 * 1 )
-            conv_33_size: (int) -- Output dimension of patt 2b ( conv 3 * 3 )
-            conv_55_reduce_size: (int) -- Output dimension of patt 3a ( conv 1 * 1 )
-            conv_55_size: (int) -- Output dimension of patt 3b ( conv 5 * 5 )
-            pool_conv_size: (int) -- Output dimension of patt 4b ( conv 1 * 1 )
+            conv_11_size: (int) -- Output dimension of Path 1 ( conv 1 * 1 )
+            conv_33_reduce_size: (int) -- Output dimension of Path 2a ( conv 1 * 1 )
+            conv_33_size: (int) -- Output dimension of Path 2b ( conv 3 * 3 )
+            conv_55_reduce_size: (int) -- Output dimension of Path 3a ( conv 1 * 1 )
+            conv_55_size: (int) -- Output dimension of Path 3b ( conv 5 * 5 )
+            pool_conv_size: (int) -- Output dimension of Path 4b ( conv 1 * 1 )
 
         Returns:
             concat: (Tensor) -- Output tensor of inception layer
         """
-        # Part 1
+        # Path 1
         conv_11 = tf.layers.conv2d(inputs, conv_11_size, [1, 1])
 
-        # Part 2
+        # Path 2
         conv_33_reduce = tf.layers.conv2d(inputs, conv_33_reduce_size, [1, 1])
         conv_33 = tf.layers.conv2d(conv_33_reduce, conv_33_size, [3, 3])
 
-        # Part 3
+        # Path 3
         conv_55_reduce = tf.layers.conv2d(inputs, conv_55_reduce_size, [1, 1])
         conv_55 = tf.layers.conv2d(conv_55_reduce, conv_55_size, [5, 5])
 
-        # Part 4
+        # Path 4
         pool = tf.layers.max_pooling2d(inputs, [3, 3], stride=1)
         conv_pool = tf.layers.conv2d(pool, pool_conv_size, [1, 1])
 
